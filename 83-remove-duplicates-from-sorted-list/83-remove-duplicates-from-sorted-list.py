849b5c668a72608cd5968b6ab7b4e0491095188a
ankit1118 <75828322+ankit1118@users.noreply.github.com>
2022-08-07 21:11:49 +0530

    Time: 55 ms (72.87%), Space: 13.9 MB (71.55%) - LeetHub

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        cur = head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next     # skip duplicated node
+            cur = cur.next     # not duplicate of current node, move to next node
+        return head
+        
